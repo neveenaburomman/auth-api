32aeccafba3828163de811945527a7dd0ab43b02
Neveen97 <neveenaburomman1@gmail.com>
2022-03-13 23:41:12 +0300

    not finished

---
+++ src/middleware/basicAuth.middleware.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const base64 = require('base-64');
+const {Users}=require('../../index');
+
+
+async function basicAuth(req,res,next) {
+
+    if(req.headers['authorization']) {
+
+        let basicHeaderParts= req.headers.authorization.split(' ');
+
+        console.log(basicHeaderParts);
+
+        let encodedPart = basicHeaderParts.pop(); //encoded(username:password)
+          console.log(encodedPart);
+          
+        let decoded = base64.decode(encodedPart); //username:password
+        console.log(decoded);
+
+        let [username,password]= decoded.split(':'); //[username: password]
+        // console.log('username');
+        
+        try {
+            let user = await Users.authenticateBasic(username,password);
+
+            req.user=user;
+            next();
+            
+        } catch(error) {
+            next(error.message);
+        }
+    }
+}
+
+module.exports = basicAuth;
---
+++ src/models/users.model.js
@@ -0,0 +1,82 @@
+'use strict'
+
+require('dotenv').config();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const SECRET = process.env.SECRET;
+
+const UsersModel = (sequelize, DataTypes) => {
+
+const Users = sequelize.define('user', {  // a define method to mappings between a model and a table . Sequelize will then automatically add 
+                                         //the attributes createdAt and updatedAt
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+        // unique: true
+    },
+
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    role:{
+        type: DataTypes.ENUM('admin', 'writer', 'editor', 'user'),
+        defaultValue: 'user',
+
+    },
+
+    token :{
+      type: DataTypes.VIRTUAL
+    },
+
+    actions: {
+        type: DataTypes.VIRTUAL,
+        get() {
+            const acl = {
+                user: ['read'],
+                writer: ['read', 'create'],
+                editor: ['read', 'create', 'update'],
+                admin: ['read', 'create', 'update', 'delete'],
+            }
+            return acl[this.role];
+        }
+    }
+
+});
+Users.authenticateBasic = async function (username,password) {
+    try {
+        const user = await this.findOne({ where :{username:username}});
+        const valid = await bcrypt.compare(password,user.password);
+        if(valid) {
+            // generate a new token
+            let newToken = jwt.sign({username:user.username},SECRET);
+            user.token = newToken;
+            return user;
+        } else {
+            console.log('user is not valid');
+            // return;
+            throw new Error('Invalid password');
+        }
+    } catch(error) {
+       console.log('error ',error);
+    }
+}
+
+Users.validateToken = async function(token) {
+    const parsedToken = jwt.verify(token,SECRET);
+    console.log('llllllll',parsedToken);
+    const user = await this.findOne({where:{username:parsedToken.username}});
+    if(user) {
+        return user
+    }
+    
+    throw new Error('invalid token')
+}
+
+return Users;
+}
+
+
+module.exports = UsersModel;
